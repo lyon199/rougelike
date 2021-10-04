b2370e9764d7e9abbb1c0c9df3396453ccb81f4a
lyon199 <91912054+lyon199@users.noreply.github.com>
2021-10-04 13:36:50 -0500

    Add files via upload

---
+++ Dungeon_Crawl/ASCII_Engine/renderer.py
@@ -0,0 +1,10 @@
+import os, gameMap
+
+USER_STOP = ""
+
+os.system("clear")
+
+#USER_STOP = input("Please make the terminal full screen. Hit enter to continue.")
+
+gameMap.create_game()
+gameMap.print_map()
---
+++ Dungeon_Crawl/dungeon.py
@@ -0,0 +1,2 @@
+class DungeonRoom:
+    pass
